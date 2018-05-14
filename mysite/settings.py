ef28056aa0619fa93224dd91b6160e397b52a93d
Mark <mmark3214@gmail.com>
2018-05-14 10:41:31 +0300

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,6 +106,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
