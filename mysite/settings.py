79fc79818b8bac63a5342c2d2f83d76970833adb
Mark <mmark3214@gmail.com>
2018-05-14 10:35:32 +0300

    local settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,18 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -125,6 +113,17 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] = dj_database_url.config()
+# DEBUG = False
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+
+
+# ##############################################
+# For work with development/production settings
+# ##############################################
+try:
+    from .local_settings import *
+except ImportError:
+    pass
