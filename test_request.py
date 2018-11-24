d038d35ba6f80af8032b696628c7035ab271631a
Mark <mmark3214@gmail.com>
2018-11-24 20:30:48 +0200

    'esponse'

--- test_request.py
+++ test_request.py
@@ -1,11 +1,11 @@
 import requests
 
-# BASE_URL = 'https://smartmark01.herokuapp.com'
-BASE_URL = 'http://localhost:8000'
+BASE_URL = 'https://smartmark01.herokuapp.com'
+# BASE_URL = 'http://localhost:8000'
 
 def main():
     # WRITE DATA
-    response = requests.post(BASE_URL + '/store', data={'V': 220, 'A': 2, 'W': 440, "socket_id": 1})
+    response = requests.post(BASE_URL + '/store', data={'V': 22, 'A': 3, 'W': 4, "socket_id": 1})
     print(response.content)
     
     # READ DATA from Server
